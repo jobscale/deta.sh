a7b1ef61dc0ca5a3f868a2b7b1f54940307eb71a
jobscale <jobscale@users.noreply.github.com>
2022-12-01 03:19:42 +0900

    eslintrc updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: 'airbnb-base',
   rules: {
     indent: ['error', 2, { MemberExpression: 0 }],
+    'no-trailing-spaces': 'error',
     'arrow-parens': 'off',
     'class-methods-use-this': 'off',
     'no-await-in-loop': 'off',
